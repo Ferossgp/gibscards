2acc1b9166d3d6037d793774775bae7ed6278e0e
Gheorghe Pinzaru <feross95@gmail.com>
2024-02-24 15:56:57 -0700

    f

--- apps/web/app/components/ui/button.tsx
+++ apps/web/app/components/ui/button.tsx
@@ -7,7 +7,6 @@ import { cn } from "~/lib/utils"
 const buttonVariants = cva(
   cn("inline-flex w-full h-full items-center justify-center whitespace-nowrap rounded-xl text-base font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
     "transition-all duration-200 ease-in-out transform -translate-y-1 -translate-x-1 hover:translate-x-0 hover:translate-y-0 disabled:translate-x-0 disabled:translate-y-0",),
-
   {
     variants: {
       variant: {
@@ -15,7 +14,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border-2 border-neutral-900 bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-[#6b66da] text-primary-foreground border-2 border-neutral-900 hover:bg-[#6b66da]/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
@@ -40,9 +39,17 @@ const buttonVariants = cva(
 )
 
 const containerVariants = cva(
-  "flex bg-neutral-800",
+  "",
   {
     variants: {
+      variant: {
+        default: "flex bg-neutral-800",
+        destructive: "flex bg-neutral-800",
+        outline: "",
+        secondary: "flex bg-neutral-800",
+        ghost: "",
+        link: "",
+      },
       size: {
         default: "rounded-2xl pb-1 pr-1",
         sm: "rounded-xl pb-0 pr-0",
@@ -51,6 +58,7 @@ const containerVariants = cva(
       },
     },
     defaultVariants: {
+      variant: "default",
       size: "default",
     },
   }
@@ -67,7 +75,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, selected, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     return (
-      <div className={cn(containerVariants({ size }))}>
+      <div className={cn(containerVariants({ variant, size }))}>
         <Comp
           className={cn(buttonVariants({ variant, size, selected, className }))}
           ref={ref}
